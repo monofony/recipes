2620336a784da286dbbeea87ee131cf910078e51
Loïc Frémont <lc.fremont@gmail.com>
2025-01-09 11:24:19 +0100

    Redo CQRS meta

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -20,7 +20,7 @@ return static function (MBConfig $mbConfig): void {
     $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
     $services->set(TagVersionReleaseWorker::class);
     $services->set(PushTagReleaseWorker::class);
-    $services->set(SetNextMutualDependenciesReleaseWorker::class);
+    // $services->set(SetNextMutualDependenciesReleaseWorker::class);
     // $services->set(UpdateBranchAliasReleaseWorker::class);
     // $services->set(PushNextDevReleaseWorker::class);
 };
